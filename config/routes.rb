812f254042d5a3f73f32df4f9f1d19f6ccb153c5
Miyauchi <hy.miyauchi@gmail.com>
2022-03-06 17:04:59 +0900

    【add】gem'graphql','graphiql-rails'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "blogs#index"
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
+  end
 end
